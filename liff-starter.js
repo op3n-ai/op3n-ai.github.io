5c0880fedc8c1b7b2d8e824271358abe280b94f7
Nattawut Ruangvivattanaroj <nattawut.ru@gmail.com>
2019-01-28 20:30:54 +0000

    change con

--- liff-starter.js
+++ liff-starter.js
@@ -52,9 +52,9 @@ function getP(){
         makeImage();
         makeVideo();
         makeAudio();
+        makeFlex();
         makeSticker();
         meProfile();
-        makeFlex();
     }
     }
 
